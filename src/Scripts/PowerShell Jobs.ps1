95905c9afa2d3f6181797054d02236eae226c51e
Paul Howard Cassidy <qawarrior+github@gmail.com>
2014-10-01 09:35:06 -0700

    Sonora Developer Meet Up
    Content used in 9-30-2014 presentation

---
+++ src/Scripts/PowerShell Jobs.ps1
@@ -0,0 +1,9 @@
+﻿Get-Process -Name powershell
+$PSJob = Start-Job -Name PSJob -ScriptBlock { $appdomain = [System.AppDomain]::CurrentDomain.GetType() ; $appdomain.GetType() ; write-output $appdomain }
+$PSJob | Format-List
+$PSJob | Receive-Job
+Get-Process -Name powershell
+$PSJob | Stop-Job
+Get-Job
+$PSJob | Remove-Job
+Get-Job
